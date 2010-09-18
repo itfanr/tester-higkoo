b4d5f9ae94e496fe0ea593f3ff9374c8a80d366b
higkooHK <higkooHK@gmail.com>
2010-09-18 12:32:47 +0000

    提高用户体验，添加了悬停提示。

--- demo/login.php
+++ demo/login.php
@@ -2,6 +2,7 @@
 <!-- 演示地址：http://higkoo.host.sk/demo/login.php  -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>演示 - Demo</title></head><body>
+<script language="javascript" src="../js/dhtml_tips_1.2.js" type="text/javascript" ></script>
 <?php
 	if ($_POST['Submit']){ // 判断是否有点击登录
 		$hide = ture; 	// 执行登录则隐藏表单
@@ -16,13 +17,13 @@
 ?>
 <form <?php if ($hide) echo 'style="display:none"'; ?> id="form" name="form" method="post" action="login.php">
 	<table width="200" height="100" border="0" align="center" cellpadding="0" cellspacing="0">
-		<caption><div align="center"><strong>用户登录</strong></div></caption>
+		<caption><div align="center"><strong><a onMouseOver="javascript:cnstat_stm('测试用例：','\t1、正确的帐号密码\n\t2、错误的帐号密码\n\t3、空帐号密码',cnstat_Style[1])" onMouseOut="javascript:cnstat_htm()" >用户登录</strong></div></caption>
 		<tr>
-			<td><div align="right">帐号：</div></td>
+			<td><div align="right"><a onMouseOver="javascript:cnstat_stm('正确的帐号：','\thigkoo',cnstat_Style[1])" onMouseOut="javascript:cnstat_htm()" >帐号：</div></td>
 			<td><div align="left"><input name="username" type="text" id="username" size="10" maxlength="10" /></div></td>
 		</tr>
 		<tr>
-			<td><div align="right">密码：</div></td>
+			<td><div align="right"><a onMouseOver="javascript:cnstat_stm('正确的密码：','\tadmin',cnstat_Style[1])" onMouseOut="javascript:cnstat_htm()" >密码：</div></td>
 			<td><div align="left"><input name="password" type="password" id="password" size="10" maxlength="10" /></div></td>
 		</tr>
 		<tr>
